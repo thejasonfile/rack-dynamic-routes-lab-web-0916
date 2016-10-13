3fb153ce165744c1c17ddbfc3e69d1aacc58baa4
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-13 14:00:50 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,10 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item = env["PATH_INFO"].split("/").last #"Figs"
+      item = env["PATH_INFO"].split("/").last
       item_obj = @@items.find {|obj| obj.name == item}
 
-      if @@items.include?(item_obj)
+      if item_obj
           resp.write "#{item_obj.price}"
       else
           resp.write "Item not found"
